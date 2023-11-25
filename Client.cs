01dc5e502e5b87d280837019d326a95988b3057a
Ximera5185 <dmitrynikitenko@yandex.ru>
2023-11-25 23:17:44 +0800

    1

---
+++ Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supermarket
+{
+    internal class Client
+    {
+        private Random _random = new Random();
+
+        private int _maxValueMoney = 1000;
+        private int _minValueMoney = 100;
+
+        public Client()
+        {
+            Money = _random.Next(_minValueMoney, _maxValueMoney);
+        }
+
+        private Product Basket { get; set; }
+
+        private int Money { get; set; }
+
+        public int Pay(int purchaseAmount) 
+        {
+            Money -= purchaseAmount;
+
+            return purchaseAmount;
+        }
+    }
+}
